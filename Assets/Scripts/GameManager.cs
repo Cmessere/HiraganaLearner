d3a8bcece791ce04ec1353c7345c3c55f1fc9e31
Carlo <messerecarlo27@gmail.com>
2021-09-28 23:49:22 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public static class GameManagerStatic
+{
+	public static GameManager gameManager;
+}
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManagerStatic.gameManager = this;
+    }
+
+    public TextMeshProUGUI currentLetter;
+
+
+    private string[] wovels = new string[] { "あ", "い", "う", "え", "お" };
+    void Start()
+    {
+
+    }
+
+    public void changeLetter()
+    {
+        currentLetter.text = wovels[Random.Range(0, 5)];
+    }
+}
