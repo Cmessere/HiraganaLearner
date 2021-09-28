bfd9fc055dede24df38c5477f6498c4038226a6c
Carlo <messerecarlo27@gmail.com>
2021-09-29 01:18:13 +0200

    Completed basic game mechanics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 public static class GameManagerStatic
 {
@@ -15,37 +16,89 @@ public class GameManager : MonoBehaviour
     }
 
     public TextMeshProUGUI japaneeseLetter;
-    public TextMeshProUGUI phoneticLetter;
+    public TextMeshProUGUI phoneticLetter1;
+    public TextMeshProUGUI phoneticLetter2;
+    public TextMeshProUGUI phoneticLetter3;
+    public TextMeshProUGUI choiceResult;
 
-
-    //private string[] japaneeseWovels = new string[] { "あ", "い", "う", "え", "お" };
-    //private string[] phoneticWovels = new string[] { "a", "i", "u", "e", "o" };
-
-    //private Codex a = new Codex("あ", "a");
-    //private Codex i = new Codex("い", "i");
-    //private Codex u = new Codex("う", "u");
-    //private Codex e = new Codex("え", "e");
-    //private Codex o = new Codex("お", "o");
+    private string correctPhonem;
 
     private Codex[] codexVowels = new Codex[] { new Codex("あ", "a"), new Codex("い", "i"), new Codex("う", "u"), new Codex("え", "e"), new Codex("お", "o")};
     void Start()
     {
-        foreach(Codex el in codexVowels) { 
-            Debug.Log("Risultato:" + el.ToString());
-        }
+        changeLetter();
     }
 
     public void changeLetter()
     {
-        int randomNumber = Random.Range(0, 5);
-        japaneeseLetter.text = codexVowels[randomNumber].Letter;
-        phoneticLetter.text = codexVowels[randomNumber].Sound;
+        int correctAnswer = Random.Range(0, 5);
+        int wrongAnswer1 = Random.Range(0, 5);
+        int wrongAnswer2 = Random.Range(0, 5);
+
+        int randomButton = Random.Range(0, 3);
+        japaneeseLetter.text = codexVowels[correctAnswer].Letter;
+        correctPhonem = codexVowels[correctAnswer].Sound;
+
+        while (wrongAnswer1 == correctAnswer || wrongAnswer1 == wrongAnswer2)
+        {
+            wrongAnswer1 = Random.Range(0, 5);
+        }
+        
+        while(wrongAnswer2 == correctAnswer || wrongAnswer2 == wrongAnswer1)
+        {
+            wrongAnswer2 = Random.Range(0, 5);
+        }
+
+        Debug.Log(correctAnswer + " " + wrongAnswer1 + " " + wrongAnswer2);
+        switch (randomButton)
+        {
+            case 0:
+                phoneticLetter1.text = codexVowels[correctAnswer].Sound;
+                phoneticLetter2.text = codexVowels[wrongAnswer1].Sound;
+                phoneticLetter3.text = codexVowels[wrongAnswer2].Sound;
+                break;
+
+            case 1:
+                phoneticLetter1.text = codexVowels[wrongAnswer1].Sound;
+                phoneticLetter2.text = codexVowels[correctAnswer].Sound;
+                phoneticLetter3.text = codexVowels[wrongAnswer2].Sound;
+                break;
+
+            case 2:
+                phoneticLetter1.text = codexVowels[wrongAnswer1].Sound;
+                phoneticLetter2.text = codexVowels[wrongAnswer2].Sound;
+                phoneticLetter3.text = codexVowels[correctAnswer].Sound;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void checkChoice()
+    {
+        var go = EventSystem.current.currentSelectedGameObject;
+        string buttonText = go.GetComponent<TextMeshProUGUI>().text;
+
+        if(buttonText == correctPhonem)
+        {
+            Debug.Log("Correct");
+            choiceResult.text = "Correct!";
+        }
+        else
+        {
+            Debug.Log("False");
+            choiceResult.text = "Wrong...";
+        }
+
+        changeLetter();
     }
 }
 
+
+
 public class Codex
 {
-    // Constructor that takes no arguments:
     public Codex()
     {
         Letter = "";
@@ -65,3 +118,4 @@ public class Codex
         return Letter +" "+ Sound;
     }
 }
+
